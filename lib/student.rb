2d133ac654c94863d05412d6ee87daa3f4bbd79f
Stela Capsa <stela.capsa@gmail.com>
2020-01-12 18:30:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,10 @@
 class Student 
 attr_accessor :first_name, :last_name
 
-def initialize(first_name, last_name)
-  @first_name = first_name
-  @last_name = last_name
-end 
+# def initialize(first_name, last_name)
+#   @first_name = first_name
+#   @last_name = last_name
+# end 
 
 
 
